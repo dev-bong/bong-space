0a4885f080b667ff1358c690e5bb3a05d9af2023
dev-bong <qhddnjswo011@gmail.com>
2023-03-21 11:07:17 +0900

    🎨 python 코드 포맷팅 (black)

--- const.py
+++ const.py
@@ -1,13 +1,15 @@
 from starlette.config import Config
 
-class Const():
+
+class Const:
     """
     ### 시스템에서 사용되는 각종 상수를 관리한다.
     *  DB_URL : 데이터베이스 URL
     """
 
-    config = Config(".env") # app 디렉토리의 .env 파일 불러오기
+    config = Config(".env")  # app 디렉토리의 .env 파일 불러오기
 
     DB_URL = config("DB_URL")
+
 
 const = Const()
