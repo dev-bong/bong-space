d0fc6cdc714905fcd5d592327df29b06daa79185
dev-bong <qhddnjswo011@gmail.com>
2023-03-21 11:02:02 +0900

    🗃️ DB 모델 생성 및 alembic으로 DB 업데이트
    - 게시글, 카테고리 모델 생성
    - 생성된 모델에 맞게 DB 업데이트 (alembic)

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -6,8 +7,28 @@ from database import Base
 class User(Base):
     __tablename__ = "user"
 
-    user_id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     nickname = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
-    join_dt = Column(DateTime(timezone=True), nullable=False)
+    join_date = Column(DateTime(timezone=True), nullable=False)
     icon = Column(String, nullable=True)
+
+
+class Post(Base):
+    __tablename__ = "post"
+
+    id = Column(Integer, primary_key=True)
+    subject = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime(timezone=True), nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    user = relationship("User", backref="user_posts")
+    category_name = Column(String, ForeignKey("category.name"), nullable=True)
+    category = relationship("Category", backref="category_posts")
+
+
+class Category(Base):
+    __tablename__ = "category"
+
+    name = Column(String, primary_key=True)
+    description = Column(Text, nullable=False)
